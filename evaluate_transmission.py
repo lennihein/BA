ffcc21aedcfb0b7cc13e70676e68c0dfaeab09e7
Lenni Hein <git@lennihein.com>
2020-03-03 18:42:33 +0100

    .

--- evaluate_transmission.py
+++ evaluate_transmission.py
@@ -1,7 +1,7 @@
 from sklearn import metrics
+from scipy.stats import wasserstein_distance
 
-
-print("Results for the Transmission:")
+print("\nResults for the Transmission:")
 
 pred_file = open('pred.txt', 'r')
 pred = pred_file.readlines()
@@ -14,5 +14,9 @@ STREAM_LENGTH = len(pred)
 if len(act) != STREAM_LENGTH:
     exit(1)
 
+# print confusion matrix
 print("\n", metrics.confusion_matrix(act, pred), "\n")
 print(metrics.classification_report(act, pred))
+
+# Wasserstein
+print(f'Wasserstein-Distance: {wasserstein_distance(pred, act):.8f}')
