b05c9c9987c1a2a15cc2d71138721c9edbfb89f5
Lenni Hein <lennihein@outlook.com>
2020-07-20 16:54:38 +0200

    added demoe
    

---
+++ receive_ascii.c
@@ -0,0 +1,170 @@
+#include "lib.h"
+#include "crc.h"
+
+#define eval(x) (method?(x>threshhold?1:0):(x<=threshhold?1:0))
+
+struct field
+{
+    size_t sync[7 * 8];
+    size_t delimiter[8];
+    size_t mac_addr[6 * 8];
+    size_t mac_src[6 * 8];
+    size_t length[2 * 8];
+    size_t payload[(1500 + 4) * 8];
+};
+
+struct field frame;
+
+size_t threshhold;
+size_t method, length;
+size_t (*meassure)(void (* addr)(void));
+size_t frequency, interval;
+uint16_t length_ = 0;
+uint32_t checksum_ = 0;
+
+void* receiver(void* _);
+
+int main(int argc, char* argv[])
+{
+    if(argc == 5)
+    {
+        if(strcmp(argv[1], "-ff")==0)
+        {
+            method = FLUSH_FLUSH;
+            meassure = meassure_ff;
+        }
+        else
+        {
+            if(strcmp(argv[1], "-fr")==0)
+            {
+                method = FLUSH_RELOAD;
+                meassure = meassure_fr;
+            }
+            else
+            {
+                goto invalid;
+            }
+        }
+
+        threshhold = strtoll(argv[2], NULL, 10);
+        if(threshhold==0) goto invalid;
+
+        frequency = strtoll(argv[3], NULL, 10);
+        if(frequency==0) goto invalid;
+        // interval in nsecs
+        interval = 1000000000/frequency;
+    }
+    else
+    {
+        invalid:
+        fprintf(stderr, "usage: ./receiver [-ff/-fr] [THRESHHOLD] [FREQUENCY]\n");
+        exit(1);
+    }
+    printf("Receiving process->process, with hardwareclock-sync and ethernet frames:\n"
+           "- Frequency in Hz: %zu\n"
+           "- Method: %s\n"
+           "- Threshhold: %zu\n",
+           frequency, method == FLUSH_FLUSH ? "Flush+Flush" : "Flush+Reload", threshhold);
+
+    pthread_t r;
+    _dispatch:
+    pthread_create(&r, NULL, receiver, NULL);
+    pthread_join(r, NULL);
+
+    // retieve checksum:
+    for(int i=0; i<32; i++)
+    {
+        checksum_ |= eval(frame.payload[i + length])<< (31 - i);
+    }
+
+    // check checksum:
+    if(checksum_!=crcFast((const unsigned char*) frame.mac_addr, sizeof(size_t) * ((6 + 6 + 2) * 8 + length)))
+    {
+        fprintf(stderr, "checksum not matching\n");
+        goto _dispatch;
+    }
+
+    for(int i = 0; i < length; i++)
+    {
+        printf("%c", (char) field.payload[i])
+    }
+
+    return 0;
+}
+
+void* receiver(void* _)
+{
+    INIT_CLOCK
+    int i = 0;
+    int preamble_counter = 0;
+    while(1)
+    {
+        sched_yield();
+        size_t d = meassure(function + 64);
+        d = eval(d);
+
+        if(preamble_counter < 7 * 8 + 7)
+        {
+            if(d == preamble_counter % 2)
+            {
+                // wrong sync bit
+                preamble_counter = 0;
+            }
+            else
+            {
+                // correct sync bit
+                preamble_counter++;
+            }
+        }
+        else
+        {
+            if(d == 1)
+            {
+                // preamble done
+                WAIT_FOR_CLOCK
+                break;
+            }
+            else
+            {
+                // still in Sync
+                preamble_counter -= 2;
+            }
+        }
+
+        WAIT_FOR_CLOCK
+    }
+
+    for(int j = 0; j < (6 + 6 + 2) * 8; j++)
+    {
+        sched_yield();
+        size_t d = meassure(function + 64);
+
+        frame.mac_addr[j] = d;
+        WAIT_FOR_CLOCK;
+    }
+
+    // retrieve length
+    for(int i=0; i<16; i++)
+    {
+        length_ |= eval(frame.length[i])<<(15-i);
+    }
+
+    if(length > 1500 && length < 46)
+    {
+        fprintf("Length faulty: %d announced\n", length);
+        exit(1);
+    }
+
+    while(1)
+    {
+        sched_yield();
+        size_t d = meassure(function + 64);
+
+        frame.payload[i++] = d;
+
+        if(i == length)
+            return NULL;
+
+        WAIT_FOR_CLOCK
+    }
+}
---
+++ send_ascii.c
@@ -0,0 +1,134 @@
+#include "lib.h"
+#include "crc.h"
+
+struct field
+{
+    size_t sync[7 * 8];
+    size_t delimiter[8];
+    size_t mac_addr[6 * 8];
+    size_t mac_src[6 * 8];
+    size_t length[2 * 8];
+    size_t payload[(1500 + 4) * 8];
+};
+
+struct field frame;
+
+struct state
+{
+    size_t output;
+    size_t cond;
+    size_t message_length;
+    size_t frame_length;
+    size_t interval;
+    size_t counter;
+};
+
+struct state state;
+size_t length, frequency, interval;
+
+void signal_handler(int _);
+
+int main(int argc, char* argv[])
+{
+    if(argc == 3)
+    {
+        frequency = strtoll(argv[1], NULL, 10);
+        // interval in usecs
+        interval = 1000000/frequency;
+
+        // converting string to arr of size_t
+        length = strlen(argv[2]);
+        if(length > 1500 && length < 46)
+        {
+            fprintf("Text must be between 46 and 1500 characters\n");
+            exit(1);
+        }
+        for(int i = 0; i < length; i++)
+        {
+            field.payload[i] = argv[2][i];
+        }
+    }
+    else
+    {
+        invalid:
+        fprintf(stderr, "usage: ./sender [FREQUENCY] [ASCII]\n");
+        exit(1);
+    }
+    state.message_length = length;
+    state.interval = interval;
+    state.cond = 1;
+    state.counter = 0;
+    state.output = 0;
+    state.frame_length = (7 + 1 + 6 + 6 + 2) * 8 + length + 4 * 8;
+
+    printf("Sending thread-thread, with hardwareclock-sync and ethernet frames:\n"
+           "- Frequency in Hz: %zu\n"
+           "- Message: %s\n",
+           frequency, argv[2]);
+
+
+    // set sync (and part of delimit)
+    memset(frame.sync, 0lu, 8 * 8 * sizeof(size_t));
+    for(int i = 0; i < 8 * 8; i += 2)
+    {
+        frame.sync[i] = 1;
+    }
+
+    // set delimiter
+    frame.delimiter[7] = 1;
+
+    // setting addresses not required here
+    
+    // set length
+    uint16_t length = state.message_length;
+    // sending the length little-endian
+    for(int i = 0; i < 16; i++)
+    {
+        frame.length[i] = (length>>(15-i)) & 0x1;
+    }
+
+    // set checksum
+    crc checksum = crcFast((const unsigned char*)frame.mac_addr, sizeof(size_t) * ((6+6+2)*8 + state.message_length));
+    // sending the checksum little-endian
+    for(int i = 0; i < 32; i++)
+    {
+        frame.payload[state.message_length+i] = (checksum>>(31-i)) & 0x1;
+    }
+
+    // start signal handler
+    signal(SIGALRM, signal_handler);
+    // alarm every communication cycle
+    ualarm(interval, interval);
+
+    loop:
+
+    while(state.cond)
+    {
+        if(state.output)
+        {
+            maccess(function + 64);
+        }
+        else
+        {
+            flush(function + 64);
+        }
+    }
+
+    printf("Sent over frame\n");
+    state.cond = 1;
+    state.counter = 0;
+    goto loop;
+
+}
+
+void signal_handler(int _)
+{
+    // update symbol
+    state.output = frame.sync[state.counter];
+    state.counter++;
+    // check if frame is fully sent
+    if(state.counter == state.frame_length)
+    {
+        state.cond = 0;
+    }
+}
