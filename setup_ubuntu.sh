1cb2edd89c3ec3c1ae44c97fc48fa8b3d780d177
Lenni Hein <git@lennihein.com>
2020-07-29 10:28:26 +0200

    .

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -3,7 +3,3 @@ apt install cmake python3-pip -y
 pip3 install numpy matplotlib sklearn
 cmake .
 make
-taskset 0x1 ./meassure_send 1000 &
-taskset 0x1 ./meassure_recv 1000 10000
-pkill meassure_send
-python3 evaluate_threshholds.py
